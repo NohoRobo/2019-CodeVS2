6994c334506f043c10232235e97dcf440828b205
Tom-Pan <30301917+Tom-Pan@users.noreply.github.com>
2019-02-04 19:23:41 -0800

    Set up RobotMap

--- nohorobo2019code/src/main/java/frc/robot/OI.java
+++ nohorobo2019code/src/main/java/frc/robot/OI.java
@@ -9,7 +9,7 @@ package frc.robot;
 
 /**
  * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
+ * interface to the commands and and command groups that allow control of the robot.
  */
 public class OI {
   //// CREATING BUTTONS
