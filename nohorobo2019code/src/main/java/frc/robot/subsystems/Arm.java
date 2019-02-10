db4795a553ac8d386a0a13271adbe125a5ec0542
AedenGB <scruffyhead@ymail.com>
2019-02-09 20:44:17 -0800

    Basic stuff in Arm.java

--- nohorobo2019code/src/main/java/frc/robot/subsystems/Arm.java
+++ nohorobo2019code/src/main/java/frc/robot/subsystems/Arm.java
@@ -7,18 +7,38 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
+//import robot.RobotMap;//this doesnt work
 
 /**
  * Add your docs here.
  */
 public class Arm extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+
+
+  
+  private static final int ARM_LEFT = 0;
+  private static final int ARM_RIGHT = 0;
+  private static final int ARM_FORWARD = 0;
+
+  private int ARM_FRONT_POSITION;
+  private int ARM_SIDE_POSITION;
+  private int ARM_BACK_POSITION;
+
+  
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
+
+  public void setArmMotor(double power){
+    
+  }
+
+  public int getArmPosition(){
+    return 0;
+  } 
 }
