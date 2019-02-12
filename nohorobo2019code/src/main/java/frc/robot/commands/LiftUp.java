2c7916acc1d917773095ccf6919e1fb88283768d
nikaash <nikaashnigam@gmail.com>
2019-02-12 10:17:47 -0800

    command list

--- nohorobo2019code/src/main/java/frc/robot/commands/LiftUp.java
+++ nohorobo2019code/src/main/java/frc/robot/commands/LiftUp.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+//what is liftdown???
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
