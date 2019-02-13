e74370931dee1c0f5715ad19179a2260a4d465b0
nikaash <nikaashnigam@gmail.com>
2019-02-12 17:23:27 -0800

    finished intake commands (not the group one)

--- nohorobo2019code/src/main/java/frc/robot/commands/IntakeRollerSolenoidIn.java
+++ nohorobo2019code/src/main/java/frc/robot/commands/IntakeRollerSolenoidIn.java
@@ -24,6 +24,8 @@ public class IntakeRollerSolenoidIn extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+
+    Robot.intake.RetractRollerSolenoid();
     
   }
 
